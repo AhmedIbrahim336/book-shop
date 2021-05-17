e7933a80d88b13f96bef097454f169cd4827f7d3
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-17 17:16:46 +0200

    Refractor the code

--- pages/tests.py
+++ pages/tests.py
@@ -5,22 +5,25 @@ from django.test import SimpleTestCase
 from django.urls.base import reverse
 
 class HomepageTests(SimpleTestCase):
+
+    # Dont' repeat your self -> get the response and store it in a property in the self class 
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get('/')
+
     # test the url 
-    def test_homepage_status_code(slef):
+    def test_homepage_status_code(self):
         # test the home page
-        response = slef.client.get('/')
-        slef.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
     def test_homepage_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(self.response.status_code,200)
 
     def test_homepage_template(self):
-        response = self.client.get('/') 
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_contains_html(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'Homepage')
+        self.assertContains( self.response, 'Homepage')
+
     def test_homepage_not_contain_html(self):
-        response =  self.client.get('/')
-        self.assertNotContains('Hi, there~ I should not be on the page')
+        self.assertNotContains(self.response , 'Hi, there I should not be on the page')
