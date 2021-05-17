43ac0f3aee5e2d87342c696ea31cb6b8fb5ada13
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-17 17:12:34 +0200

    Test hte page content

--- pages/tests.py
+++ pages/tests.py
@@ -17,3 +17,10 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_template(self):
         response = self.client.get('/') 
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Homepage')
+    def test_homepage_not_contain_html(self):
+        response =  self.client.get('/')
+        self.assertNotContains('Hi, there~ I should not be on the page')
